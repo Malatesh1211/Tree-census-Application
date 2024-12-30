411e23f830f4952e18053ad20192bb200228f51e
Malatesh1211 <malatesh989@gmail.com>
2024-12-30 18:01:35 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -8,18 +8,32 @@ const urlsToCache = [
     "icon-512x512.png"
 ];
 
-self.addEventListener("install", (event) => {
+// service-worker.js
+self.addEventListener('install', (event) => {
+    console.log('Service Worker installing.');
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(urlsToCache);
+        caches.open('tree-census-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/styles.css',
+                '/script.js',
+                '/icon-192x192.png',
+                '/icon-512x512.png'
+            ]);
         })
     );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('activate', (event) => {
+    console.log('Service Worker activating.');
+});
+
+self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
             return response || fetch(event.request);
         })
     );
 });
+
