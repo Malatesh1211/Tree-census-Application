75f53fb4bbbdb71b91ac7582c316dfe4d992fe50
Malatesh1211 <malatesh989@gmail.com>
2024-12-30 18:18:13 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,50 @@
 const CACHE_NAME = "tree-census-cache";
 const urlsToCache = [
-    "index.html",
-    "main.js",
-    "manifest.json",
-    "style.css",
-    "app.js",
-    "icon-192x192.png",
-    "icon-512x512.png"
+    "/",
+    "/index.html",
+    "/main.js",
+    "/manifest.json",
+    "/styles.css",
+    "/script.js",
+    "/icon-192x192.png",
+    "/icon-512x512.png"
 ];
 
-// service-worker.js
+// Install Event
 self.addEventListener('install', (event) => {
     console.log('Service Worker installing.');
     event.waitUntil(
-        caches.open('tree-census-cache').then((cache) => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/main.js',
-                '/styles.css',
-                '/script.js',
-                '/icon-192x192.png',
-                '/icon-512x512.png'
-            ]);
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Opened cache:', CACHE_NAME);
+            return cache.addAll(urlsToCache);
         })
     );
 });
 
+// Activate Event
 self.addEventListener('activate', (event) => {
     console.log('Service Worker activating.');
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        console.log('Deleting old cache:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
 
+// Fetch Event
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
+            // Return cache response if found, otherwise fetch from network
             return response || fetch(event.request);
         })
     );
 });
-
